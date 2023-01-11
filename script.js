bd9b18a744c2a9b65dc66ec47f2c87740ce3594e
MindulaHewadewage <mindufernando@gmail.com>
2023-01-11 15:37:17 +0100

    hardcoded la lista

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log ('JS OK')
+const app = Vue.createApp({
+    data(){
+        return {
+            tasks:[
+                'Fare i compiti',
+                'Fare la spesa',
+                'Fare il bucato'
+            ]
+        }
+    }
+});
+
+
+app.mount('#root');
